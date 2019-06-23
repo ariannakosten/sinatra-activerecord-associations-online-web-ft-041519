710ac92bb526ebff01a6dc0ee4d0b84ae8d2895a
Arianna Kosten <arianna.kosten@gmail.com>
2019-06-23 08:27:15 +0000

    Automatically backed up by Learn

--- db/migrate/20150915202628_create_cats.rb
+++ db/migrate/20150915202628_create_cats.rb
@@ -1,23 +1,23 @@
-# class CreateCats < ActiveRecord::Migration
-#   def up
-#     create_table :cats do |t|
-#       t.string :name
-#       t.integer :age
-#       t.string :breed
-#     end
-#   end
-  
-#   def down
-#     drop_table :cats
-#   end
-# end
-
-class CreateCats < ActiveRecord::Base
-  def change
+class CreateCats < ActiveRecord::Migration
+  def up
     create_table :cats do |t|
       t.string :name
       t.integer :age
       t.string :breed
     end
   end
+  
+  def down
+    drop_table :cats
+  end
 end
+
+# class CreateCats < ActiveRecord::Base
+#   def change
+#     create_table :cats do |t|
+#       t.string :name
+#       t.integer :age
+#       t.string :breed
+#     end
+#   end
+# end
